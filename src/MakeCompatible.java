86b185df142df79fdb5d31f32fef4e4ad6f2eef4
Oliver Ye <olizye@gmail.com>
2018-03-28 20:28:03 +0200

    Finalizado Makecompatible, cambios en TransformRaw

--- src/MakeCompatible.java
+++ src/MakeCompatible.java
@@ -1,6 +1,11 @@
+import java.io.File;
+
+import weka.classifiers.Classifier;
 import weka.core.Instances;
 import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.FixedDictionaryStringToWordVector;
 import weka.filters.unsupervised.attribute.Standardize;
+import weka.filters.unsupervised.attribute.StringToWordVector;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class MakeCompatible {
@@ -21,34 +26,30 @@ public class MakeCompatible {
 					"Este programa tiene como función hacer que un conjunto de evaluaci�n dado sea compatible con un conjunto de entrenamiento dado.");
 			System.out.println("Este programa necesita que introduzcas 3 argumentos para funcionar correctamente.");
 			System.out.println(
-					"PRECONDICIONES:\nEl primer argumento será el path del conjunto de entrenamiento con el que comparar. "
-							+ "El segundo es el path del conjunto de evaluaci�n que se debe hacer compatible. Mientras que el tercero ser� el path de la salida para este nuevo conjunto de datos compatible.");
-			System.out.println(
-					"POSTCONDICIONES:\nEl resultado de esta aplicación será la creación de un fichero .arff "
-							+ "que contiene un conjunto de datos de evauaci�n compatible con el del entrenamiento\n");
+					"PRECONDICIONES:\nEl primer argumento será el path del diccionario. "
+							+ "El segundo es el path del conjunto de evaluaci�n que se debe hacer compatible. El tercero ser� el path de la salida para este nuevo conjunto de datos compatible");
+			System.out.println("POSTCONDICIONES:\nEl resultado de esta aplicación será la creación de un fichero .arff "
+					+ "que contiene un conjunto de datos de evauaci�n compatible con el del entrenamiento\n");
 			System.out.println("Lista de argumentos:\n" + "-- Path del conjunto de entrenamiento con el que comparar."
 					+ "\n-- Path de la raíz del fichero .arff de evaluaci�n a convertir."
 					+ "\n-- Path del destino donde se guardará el fichero .arff resultante tras la ejecución");
 			System.out.println(
-					"Ejemplo de una correcta ejecución: java -jar MakeCompatible.jar /path/to/train.arff /path/to/test.arff /path/to/newArff.arff");
+					"Ejemplo de una correcta ejecución: java -jar MakeCompatible.jar /path/to/train.arff /path/to/test.arff /path/to/newArff.arff /path/to/diccionario");
 			System.exit(0);
 		} else if (args.length != 3) {
 			System.out.println("Error en el input. Revise su sintaxis.");
 			System.exit(1);
 		} else if (args.length == 3) {
-			String pathBoW = args[0];
+			String pathDicc = args[0];
 			String pathIn = args[1];
 			String pathOut = args[2];
-			DataSource source0 = new DataSource(pathBoW);
 			DataSource source1 = new DataSource(pathIn);
 
-			Instances train = null;
 			Instances test = null;
 			try {
-				train = source0.getDataSet();
+
 				test = source1.getDataSet();
-				if (train.classIndex() == -1) {
-					train.setClassIndex(train.numAttributes() - 1);
+				if (test.classIndex() == -1) {
 					test.setClassIndex(test.numAttributes() - 1);
 				}
 			} catch (NullPointerException e) {
@@ -56,14 +57,11 @@ public class MakeCompatible {
 				System.exit(1);
 			}
 
-			Standardize filter = new Standardize();
-			try {
-				filter.setInputFormat(train);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			Instances newTrain = Filter.useFilter(train, filter);
-			Instances newTest = Filter.useFilter(test, filter);
+			FixedDictionaryStringToWordVector fixedFilter = new FixedDictionaryStringToWordVector();
+			fixedFilter.setDictionaryFile(new File(pathDicc));
+			fixedFilter.setInputFormat(test);
+			Instances newTest = Filter.useFilter(test, fixedFilter);
+
 			Utilities.saveArff(newTest, pathOut);
 		}
 	}
--- src/TransformRaw.java
+++ src/TransformRaw.java
@@ -1,5 +1,4 @@
 
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -7,131 +6,127 @@ import java.io.IOException;
 
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
+import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.*;
 import weka.filters.unsupervised.instance.NonSparseToSparse;
 
 public class TransformRaw {
 	/**
-	 * Transforma el espacio de atributos del conjunto de entrenamiento a BoW
-	 * o TF·IDF
+	 * Transforma el espacio de atributos del conjunto de entrenamiento a BoW o
+	 * TFï¿½IDF
 	 *
 	 * @param args
-	 *            Parámetros de entrada
+	 *            Parï¿½metros de entrada
 	 * @throws Exception
 	 *             Si no se pueden obtener correctamente las instancias a partir de
 	 *             los ficheros de entrada
 	 */
 	public static void main(String[] args) throws Exception {
-		
+
 		if (args.length == 0) {
 			System.out.println("=====TRANSFORM RAW=====");
 			System.out.println(
-					"Este programa tiene como función transformar el espacio de atributos del conjunto de entrenamiento a BoW o TF·IDF.");
-			System.out.println("Este programa necesita que introduzcas 4 argumentos para funcionar correctamente.");
+					"Este programa tiene como funciï¿½n transformar el espacio de atributos del conjunto de entrenamiento a BoW o TFï¿½IDF.");
+			System.out.println("Este programa necesita que introduzcas 5 argumentos para funcionar correctamente.");
 			System.out.println(
-					"PRECONDICIONES:\nEl primer argumento será el path del conjunto de entrenamiento a transformar. "
-							+ "El segundo es el path destino. El tercero indicará si queremos transformar a BoW o TF·IDF, los valores válidos: 'BOW' , 'TF·IDF'."
-							+ "Y el cuarto indicará si queremos una representación Sparse o NonSparse, los valores válidos: 'Sparse' , 'NonSparse'");
+					"PRECONDICIONES:\nEl primer argumento serï¿½ el path del conjunto de entrenamiento a transformar. "
+							+ "El segundo es el path destino. El tercero indicarï¿½ si queremos transformar a BoW o TFï¿½IDF, los valores vï¿½lidos: 'BOW' , 'TFï¿½IDF'."
+							+ "El cuarto indicarï¿½ si queremos una representaciï¿½n Sparse o NonSparse, los valores vï¿½lidos: 'Sparse' , 'NonSparse'"+
+							"Y el Ãºltimo serÃ¡ el path donde guardar el diccionario generado");
 			System.out.println(
-					"POSTCONDICIONES:\nEl resultado de esta aplicación será una representación BoW o TF·IDF del espacio de atributos del conjunto de entrenamiento ");
+					"POSTCONDICIONES:\nEl resultado de esta aplicaciï¿½n serï¿½ una representaciï¿½n BoW o TFï¿½IDF del espacio de atributos del conjunto de entrenamiento ");
 			System.out.println("Lista de argumentos:\n" + "-- Path del conjunto de entrenamiento a transformar."
-					+ "\n-- Path de salida."
-					+ "\n-- Opción BoW o TF·IDF"
-					+ "\n-- Opción Sparse o NonSparse");
+					+ "\n-- Path de salida." + "\n-- Opciï¿½n BoW o TFï¿½IDF" + "\n-- Opciï¿½n Sparse o NonSparse"+"\n-- Path del diccionario generado");
 			System.out.println(
-					"Ejemplo de una correcta ejecución: java -jar TransformRaw.jar /path/to/train.arff /path/to/trainBOW.arff BOW NonSparse");
+					"Ejemplo de una correcta ejecuciï¿½n: java -jar TransformRaw.jar /path/to/train.arff /path/to/trainBOW.arff BOW NonSparse /path/to/diccionario");
 			System.exit(0);
-		} 
-		else{
-						Instances data = cargarFichero(args[0]);
-						data.setClassIndex(data.numAttributes()-1);
-						
-						StringToWordVector filter = null;
-						Instances dataFiltered = null;
-				/**
-				 * Transformamos el arff raw a BOW.
-				 */
-				if(args[2].equals("BOW")){
-					filter = new StringToWordVector();
-					filter.setInputFormat(data);
-					filter.setIDFTransform(false);
-					filter.setTFTransform(false);
-					dataFiltered = filter.useFilter(data,filter);
-				
-				}
-				/**
-				 * Transformamos el arff raw a TF·IDF
-				 */
-				else{
-					filter = new StringToWordVector();
-					filter.setInputFormat(data);
-					filter.setIDFTransform(true);
-					filter.setTFTransform(true);
-					filter.setOutputWordCounts(true);
-					dataFiltered = filter.useFilter(data,filter);
-				}
-				
-				/**
-				 * Aplicamos el filtro NonSparseToSparse.
-				 */
-				if(args[3].equals("Sparse"))
-				{
-					NonSparseToSparse filtroSparse = new NonSparseToSparse();
-					filtroSparse.setInputFormat(dataFiltered);
-					dataFiltered= filtroSparse.useFilter(dataFiltered, filtroSparse);
-				}
-				
-				/*
-				 * guardamos los datos en el path especificado
-				 */
-				guardarDatos(args[1],dataFiltered);
+		} else {
+			Instances data = cargarFichero(args[0]);
+			data.setClassIndex(data.numAttributes() - 1);
+			String pathDicc= args[4];
+			StringToWordVector filter = null;
+			Instances dataFiltered = null;
+			/**
+			 * Transformamos el arff raw a BOW.
+			 */
+			if (args[2].equals("BOW")) {
+				filter = new StringToWordVector();
+				filter.setInputFormat(data);
+				filter.setIDFTransform(false);
+				filter.setTFTransform(false);
+				filter.setDictionaryFileToSaveTo(new File(pathDicc));
+				dataFiltered = Filter.useFilter(data, filter);
+
+			}
+			/**
+			 * Transformamos el arff raw a TFï¿½IDF
+			 */
+			else {
+				filter = new StringToWordVector();
+				filter.setInputFormat(data);
+				filter.setIDFTransform(true);
+				filter.setTFTransform(true);
+				filter.setOutputWordCounts(true);
+				filter.setDictionaryFileToSaveTo(new File(pathDicc));
+				dataFiltered = Filter.useFilter(data, filter);
+			}
+
+			/**
+			 * Aplicamos el filtro NonSparseToSparse.
+			 */
+			if (args[3].equals("Sparse")) {
+				NonSparseToSparse filtroSparse = new NonSparseToSparse();
+				filtroSparse.setInputFormat(dataFiltered);
+				dataFiltered = Filter.useFilter(dataFiltered, filtroSparse);
+			}
+
+			/*
+			 * guardamos los datos en el path especificado
+			 */
+			guardarDatos(args[1], dataFiltered);
 		}
-		
-		
+
 	}
-	
+
 	/**
 	 * Cargamos las instancias del fichero
-	 * @param path de entrada de ficheros
+	 * 
+	 * @param path
+	 *            de entrada de ficheros
 	 * @exception Exception
-	 *             Si no se pueden obtener correctamente las instancias a partir de
-	 *             los ficheros de entrada
+	 *                Si no se pueden obtener correctamente las instancias a partir
+	 *                de los ficheros de entrada
 	 */
-	private static Instances cargarFichero(String path) throws Exception{
+	private static Instances cargarFichero(String path) throws Exception {
 
-		
-			FileReader file = null;
-					
-					try{
-						file = new FileReader(path);
-					}
-					catch(FileNotFoundException e)
-					{
-						System.out.println("ERROR: Comprueba el path del fichero: "+path);
-					}
-					
-					Instances data = null;
-					
-					try{
-						data = new Instances(file);
-					}
-					catch(IOException e)
-					{
-						System.out.println("ERROR: Comprueba el path del fichero: "+path);
-					}
-					
-					try{
-						file.close();
-					}
-					catch(IOException e)
-					{}
+		FileReader file = null;
+
+		try {
+			file = new FileReader(path);
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR: Comprueba el path del fichero: " + path);
+		}
+
+		Instances data = null;
+
+		try {
+			data = new Instances(file);
+		} catch (IOException e) {
+			System.out.println("ERROR: Comprueba el path del fichero: " + path);
+		}
+
+		try {
+			file.close();
+		} catch (IOException e) {
+		}
 		return data;
 	}
-	
+
 	/**
 	 * guarda los datos (instancias) en la ruta especificada
-	 * @param path 
-	 * @param data 
+	 * 
+	 * @param path
+	 * @param data
 	 */
 	private static void guardarDatos(String path, Instances data) {
 		try {
