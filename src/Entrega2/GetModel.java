b433179e8f8d2fe3db30d6ecfef22c622d0c21a2
Guzmán <guzlosan@gmail.com>
2018-04-27 11:40:19 +0200

    GetModel 100% funcional.
    De nuevo este método tarda MUCHO, usar con precaución.

--- src/Entrega2/GetModel.java
+++ src/Entrega2/GetModel.java
@@ -3,14 +3,13 @@ package Entrega2;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Random;
 
+import Utilities.CommonUtilities;
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.functions.SMO;
 import weka.classifiers.functions.supportVector.PolyKernel;
 import weka.core.Instances;
-import weka.core.SerializationHelper;
 import weka.core.converters.ConverterUtils.DataSource;
 
 public class GetModel {
@@ -49,13 +48,13 @@ public class GetModel {
     private static void getModel(Instances trainedData, double atributoExponente, String pathToSaveModel,String pathToWrite) throws Exception {
 
         Classifier ClasSMO = new SMO();
-        Evaluation eval = evalKFoldCrossValidation(ClasSMO, trainedData, 10, 1);
+        Evaluation eval = CommonUtilities.evalKFoldCrossValidation(ClasSMO, trainedData, 10, 1);
         escribirResult("10-fold Cross Validation :",eval,trainedData.numAttributes() - 1,pathToWrite);
         ClasSMO.buildClassifier(trainedData);
         Evaluation eval2 = new Evaluation(trainedData);
         eval2.evaluateModel(ClasSMO, trainedData);
         escribirResult("Resubstitution error :",eval2,trainedData.numAttributes() - 1,pathToWrite);
-        saveModel(ClasSMO, pathToSaveModel);
+        CommonUtilities.saveModel(ClasSMO, pathToSaveModel);
 
     }
 
@@ -93,7 +92,7 @@ public class GetModel {
             bf.write(pText);
             bf.close();
         } catch (IOException e) {
-            printlnError(String.format("Error al escribir en %s", pPath));
+            CommonUtilities.printlnError(String.format("Error al escribir en %s", pPath));
             e.printStackTrace();
         }
     }
@@ -112,63 +111,4 @@ public class GetModel {
         model.buildClassifier(trainedData);
         return model;
     }
-    /**
-     * Guarda el clasificador pClassifier en la ruta pPath.
-     *
-     * @param pClassifier clasificador a guardar
-     * @param pPath       ruta del archivo a crear
-     *
-     */
-    private static void saveModel(Classifier pClassifier, String pPath) {
-        try {
-            SerializationHelper.write(pPath, pClassifier);
-        } catch (Exception e) {
-            printlnError("Error al guardar el clasificador");
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Escribe por consola el texto pTexto en color rojo.
-     * SIEMPRE incluye salto de línea.
-     *
-     * @param pText texto a escribir
-     */
-    private static void printlnError(String pText) {
-        printError(String.format("%s\n", pText));
-    }
-
-    /**
-     * Escribe por consola el texto pTexto en color rojo.
-     * NO incluye salto de línea.
-     *
-     * @param pText texto a escribir
-     */
-    private static void printError(String pText) {
-        System.out.print(String.format("\33[31m%s\33[0m", pText));
-    }
-
-    /**
-     * Evaluación k-Fold Cross-Validation
-     * Realiza una evaluación k-Fold Cross-Validation sobre el clasificador pClassifier con las instancias pIsntaces y la seed pSeed donde k es pFolds.
-     *
-     * @param pClassifier clasificador a evaluar
-     * @param pInstances  instancias con las que evaluar
-     * @param pFolds      número de iteraciones a realizar
-     * @param pSeed       seed para la randomizanión
-     * @return el objeto Evaluation que contiene los resultados de la evaluación, null si hay problemas al evaluar el clasificador.
-     */
-
-    private static Evaluation evalKFoldCrossValidation(Classifier pClassifier, Instances pInstances, int pFolds,
-                                                       long pSeed) {
-        Evaluation evaluation = null;
-        try {
-            evaluation.crossValidateModel(pClassifier, pInstances, pFolds, new Random(pSeed));
-        } catch (Exception e) {
-            printlnError("Error al evaluar el clasificador");
-            e.printStackTrace();
-        }
-        return evaluation;
-    }
-
 }
